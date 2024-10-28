b01198c813045258c7de9332c5058d8c32713ece
nick <lcl6659@163.com>
2024-10-28 11:01:29 +0800

    config change

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -25,47 +25,8 @@ export default defineConfig({
   themeConfig: {
     logo: "/logo.png",
     logoLink: "/",
-    // outline: {
-    //   level: [2, 3],
-    //   label: "本页导航",
-    // },
-    // lightModeSwitchTitle: "切换亮色模式",
-    // darkModeSwitchTitle: "切换暗色模式",
-    // returnToTopLabel: "返回顶部",
-    // langMenuLabel: "菜单",
-    // lastUpdated: {
-    //   text: '最后更新时间：',
-    //   formatOptions: {
-    //     dateStyle: 'full',
-    //     timeStyle: 'medium'
-    //   }
-    // },
-    // search: {
-    //   provider: 'local',
-    //   options: {
-    //     translations: {
-    //       button: {
-    //         buttonText: '搜索文档',
-    //         buttonAriaLabel: '搜索文档',
-    //       },
-    //       modal: {
-    //         noResultsText: '无法找到结果',
-    //         resetButtonTitle: '清除查询条件',
-    //         displayDetails: '显示列表详情',
-    //         footer: {
-    //           selectText: '选择',
-    //           navigateText: '切换',
-    //           closeText: '关闭',
-    //         },
-    //       },
-    //     },
-    //   }
-    // },
     nav: [
-      { text: 'Home', link: '/' },
-      // { text: 'docs', link: '/docs/', activeMatch: '/docs/' },
-      // { text: 'Cookdocs', link: '/cookdocs/', activeMatch: '/cookdocs/' },
-      // { text: '参考', link: '/reference/', activeMatch: '/reference/' },
+      { text: 'Home', link: '/', activeMatch: '/' },
     ],
 
     sidebar: {
@@ -97,15 +58,8 @@ export default defineConfig({
         // },
       ],
     },
-
-    
-
     // socialLinks: [
     //   { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
     // ],
-    // docFooter: {
-    //   prev: '上一页',
-    //   next: '下一页'
-    // }
   }
 })
